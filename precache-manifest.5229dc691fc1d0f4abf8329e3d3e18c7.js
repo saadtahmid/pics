2ba25e234da80be7bceb83f81d0b5238379139d0
Saad Tahmid <saadtahmid1890@gmail.com>
2020-06-01 22:04:36 +0600

    Updates

---
+++ precache-manifest.5229dc691fc1d0f4abf8329e3d3e18c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc664d05fac13412f28081b2408c682b",
+    "url": "/pics/index.html"
+  },
+  {
+    "revision": "5ea3a4644648e1c60c58",
+    "url": "/pics/static/css/main.6cf75743.chunk.css"
+  },
+  {
+    "revision": "576239bdd47165c3f765",
+    "url": "/pics/static/js/2.f88abdb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pics/static/js/2.f88abdb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ea3a4644648e1c60c58",
+    "url": "/pics/static/js/main.2654bbe3.chunk.js"
+  },
+  {
+    "revision": "1d26167a8e6ba9914b1c",
+    "url": "/pics/static/js/runtime-main.ee210543.js"
+  }
+]);
