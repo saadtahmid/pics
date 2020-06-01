06d4e1b4b6cd2669ab4607f84dd057b1227c532c
Saad Tahmid <saadtahmid1890@gmail.com>
2020-06-01 21:54:52 +0600

    first commit

---
+++ src/components/ImageList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ImageList.css";
+import Imagecard from "./ImageCard";
+const ImageList = (props) => {
+  const images = props.images.map((image) => (
+    <Imagecard key={image.id} image={image} />
+  ));
+  return <div className="image-list">{images}</div>;
+};
+export default ImageList;
